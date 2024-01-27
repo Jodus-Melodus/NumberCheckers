787aa1d9b8d4e2ba5ddfdcb60be1a02ad7a5de7a
Jodus-Melodus <philipgricius@gmail.com>
2024-01-27 09:17:19 +0200

    removed submit buttons

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 let usedNumbers = [];
 let playerNames = [];
 let TurnIndex = 0;
+const duration = 20000;
+let startTime;
+let timer;
+
+function gameOver(msg) {
+    updatePlayerTurn();
+    alert(msg + "\n'" + playerNames[TurnIndex] + "' wins!");
+    clearTimeout(timer);
+}
+
+function timerCallback() {
+    gameOver("Your time ran out!")
+}
+
+function getTimeRemaining() {
+    const currentTime = Date.now();
+    return Math.max(0, startTime + duration - currentTime);
+}
 
 function getRandomInRange(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
@@ -30,14 +48,14 @@ function submit() {
     var value = parseInt(document.getElementById("number").value);
 
     if (usedNumbers.includes(value)) {
-        alert("Game Over!\nThis number has already been entered.\n" + playerNames[TurnIndex] + " wins!");
+        gameOver("Game Over!\nThis number has already been entered.");
     }
     else if (value % 2 == 0) {
         usedNumbers.push(value);
         newCard(value);
     }
     else {
-        alert("Game Over!\nThe number is supposed to be an even number.\n" + playerNames[TurnIndex] + " wins!")
+        gameOver("Game Over!\nThe number is supposed to be an even number.")
     }
     updatePlayerTurn();
     document.getElementById("number").value = "";
@@ -77,4 +95,24 @@ function gotoGameWindow() {
 function startGame() {
     playerNames = JSON.parse(localStorage.getItem("playerNames"));
     updatePlayerTurn();
+
+    document.getElementById("number").addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            submit();
+        }
+    });
+
+    startTime = Date.now();
+    timer = setTimeout(timerCallback, duration);
+
+    const interval = setInterval(() => {
+        console.log("Time remaining: " + getTimeRemaining() + " milliseconds");
+    }, 1000);
+
 };
+
+document.getElementById("playername").addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        newplayer();
+    }
+});
